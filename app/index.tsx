4c369e22489c6309fcabab6444db1f6bb4050f0c
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-27 16:58:50 +0530

    assets, constants, folder structure and home bar

--- app/index.tsx
+++ app/index.tsx
@@ -6,9 +6,13 @@ import { StatusBar } from "expo-status-bar";
 const App = () => {
   return (
     <View className="flex flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
-      <StatusBar style="auto"/>
-      <Text className="pt-4"><Link href="/profile" className="text-amber-700">Go to profile</Link></Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Text className="pt-4">
+        <Link href="../tabs/home" className="text-amber-700">
+          Go to home
+        </Link>
+      </Text>
     </View>
   );
 };
