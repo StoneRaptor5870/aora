3152db7db663f61ebf6fdc40644298385051bfc5
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-30 19:48:29 +0530

    home screen and its components

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,19 @@
 import CustomButton from "./CustomButton";
 import Loader from "./Loader";
 import FormField from "./FormField";
+import Trending from "./Trending";
+import SearchInput from "./SearchInput";
+import EmptyState from "./EmptyState";
+import VideoCard from "./VideoCard";
+import InfoBox from "./InfoBox";
 
 export {
   CustomButton,
   Loader,
-  FormField
+  FormField,
+  Trending,
+  SearchInput,
+  EmptyState,
+  VideoCard,
+  InfoBox
 };
